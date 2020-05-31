df39fba0ca502d4a8128373b887433a713f11948
Simon Szalai <simon.szalai@gmail.com>
2020-05-31 09:50:29 +0200

    fixed project name

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -28,8 +28,8 @@ aws ssm put-parameter --name "DJANGO_SECRET" --value "$(< DJANGO_SECRET)" --type
 echo "DJANGO_SECRET was successfully saved to SSM parameter store."
 
 # Create SSH keypair for EC2, then make it read-only
-aws ec2 create-key-pair --key-name $1 --query 'KeyMaterial' --output text > ~/.aws/ssh_$1_ec2.pem
-chmod 400 ~/.aws/ssh_$1_ec2.pem
+aws ec2 create-key-pair --key-name sorterbot --query 'KeyMaterial' --output text > ~/.aws/ssh_sorterbot_ec2.pem
+chmod 400 ~/.aws/ssh_sorterbot_ec2.pem
 echo "SSH keypair created successfully."
 
 # Create Policy, User and Access Key for GitHub Actions
@@ -43,14 +43,14 @@ python3 ../utils/set_github_secret.py $GITHUB_TOKEN AWS_ACCESS_KEY_ID $ACCESS_KE
 python3 ../utils/set_github_secret.py $GITHUB_TOKEN AWS_SECRET_ACCESS_KEY $ACCESS_KEY_OUT | jq '.AccessKey.SecretAccessKey'
 
 # Deploy CloudFormation Stack
-PROJECT_NAME=$1 cdk deploy SorterBotProdStack --require-approval never
+PROJECT_NAME=sorterbot cdk deploy SorterBotProdStack --require-approval never
 
 # Retrieve newly created RDS instance host
-postgresHost=$(aws rds describe-db-instances --filters "Name=db-instance-id,Values=$1-postgres" --query "DBInstances[*].Endpoint.Address" --output text)
+postgresHost=$(aws rds describe-db-instances --filters "Name=db-instance-id,Values=sorterbot-postgres" --query "DBInstances[*].Endpoint.Address" --output text)
 echo "RDS Instance endpoint address retrieved."
 
 # Construct postgres connection string and save it as an SSM SecureString parameter
-PG_CONN="postgresql://postgres:$(< PG_PASS)@${postgresHost}:5432/$1"
+PG_CONN="postgresql://postgres:$(< PG_PASS)@${postgresHost}:5432/sorterbot"
 aws ssm put-parameter --name "PG_CONN" --value $PG_CONN --type "SecureString" > /dev/null
 echo "Postgres connection string created and saved to SSM parameter store."
 
@@ -66,25 +66,25 @@ rm DJANGO_SECRET
 echo "DJANGO_SECRET file deleted."
 
 # Wait until EC2 is up and running
-aws ec2 wait instance-running --filters "Name=tag-value,Values=$1-control-panel"
+aws ec2 wait instance-running --filters "Name=tag-value,Values=sorterbot-control-panel"
 echo "EC2 instance is running."
 
 # Retrieve newly created EC2 instance host (and remove whitespace)
-publicDns=$(aws ec2 describe-instances --filters "Name=tag-value,Values=$1-control-panel" --query "Reservations[*].Instances[*].PublicDnsName" --output text)
+publicDns=$(aws ec2 describe-instances --filters "Name=tag-value,Values=sorterbot-control-panel" --query "Reservations[*].Instances[*].PublicDnsName" --output text)
 publicDns="$(echo -e "${publicDns}" | tr -d '[:space:]')"
 echo "EC2 Instance DNS is retrieved."
 
 # Install dependencies on EC2 instance
-ssh -o "StrictHostKeyChecking no" -i ~/.aws/ssh_$1_ec2.pem ec2-user@"${publicDns}" "bash -s -- uno" < setup_control_panel.sh
+ssh -o "StrictHostKeyChecking no" -i ~/.aws/ssh_sorterbot_ec2.pem ec2-user@"${publicDns}" "bash -s -- uno" < setup_control_panel.sh
 echo "EC2 setup complete."
 
 # Build Docker image than start Docker Compose
-ssh -o "StrictHostKeyChecking no" -i ~/.aws/ssh_$1_ec2.pem ec2-user@"${publicDns}" "docker build -t sorterbot_control:latest sorterbot_control; cd sorterbot_control; EXT_PORT=80 DISABLE_AWS=0 MIGRATE=1 docker-compose up"
+ssh -o "StrictHostKeyChecking no" -i ~/.aws/ssh_sorterbot_ec2.pem ec2-user@"${publicDns}" "docker build -t sorterbot_control:latest sorterbot_control; cd sorterbot_control; EXT_PORT=80 DISABLE_AWS=0 MIGRATE=1 docker-compose up"
 
 # Set Django password as environment variable on the EC2 instance
-ssh -o "StrictHostKeyChecking no" -i ~/.aws/ssh_$1_ec2.pem ec2-user@"${publicDns}" "export DJANGO_SUPERUSER_PASSWORD=$3"
-ssh -o "StrictHostKeyChecking no" -i ~/.aws/ssh_$1_ec2.pem ec2-user@"${publicDns}" "sudo pip3 install -r sorterbot_control/sbc_server/requirements.txt"
-ssh -o "StrictHostKeyChecking no" -i ~/.aws/ssh_$1_ec2.pem ec2-user@"${publicDns}" "python3 sorterbot_control/sbc_server/manage.py createsuperuser --username $2 --email blank@email.com"
+ssh -o "StrictHostKeyChecking no" -i ~/.aws/ssh_sorterbot_ec2.pem ec2-user@"${publicDns}" "export DJANGO_SUPERUSER_PASSWORD=$3"
+ssh -o "StrictHostKeyChecking no" -i ~/.aws/ssh_sorterbot_ec2.pem ec2-user@"${publicDns}" "sudo pip3 install -r sorterbot_control/sbc_server/requirements.txt"
+ssh -o "StrictHostKeyChecking no" -i ~/.aws/ssh_sorterbot_ec2.pem ec2-user@"${publicDns}" "python3 sorterbot_control/sbc_server/manage.py createsuperuser --username $2 --email blank@email.com"
 
 # Collect static files and host them on S3
-ssh -o "StrictHostKeyChecking no" -i ~/.aws/ssh_$1_ec2.pem ec2-user@"${publicDns}" "python3 sorterbot_control/sbc_server/manage.py collectstatic
+ssh -o "StrictHostKeyChecking no" -i ~/.aws/ssh_sorterbot_ec2.pem ec2-user@"${publicDns}" "python3 sorterbot_control/sbc_server/manage.py collectstatic
